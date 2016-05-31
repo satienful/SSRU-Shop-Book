28f2a4b488db02d3783fbe398c3de56beaf92cae
satien <satienful@gmail.com>
2016-05-31 10:14:48 +0700

    Upload Finish

--- app/src/main/java/ssru/janpla/satien/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/ssru/janpla/satien/ssrushopbook/SignUpActivity.java
@@ -2,8 +2,19 @@ package ssru.janpla.satien.ssrushopbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
     //Explicit
@@ -42,7 +53,30 @@ public class SignUpActivity extends AppCompatActivity {
 
     }   //ClickSing
     private void uploadNewUser() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/ssru/add_user_satien.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.d("test", "e=" + e.toString());
+            }
 
-    }
+            @Override
+            public void onResponse(Response response) throws IOException {
+            finish();
+            }
+        });
+
+
+    }   //Upload
 
 }   //Main Class
